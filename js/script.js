0151a2f2a2e5b9820e0079a6ac41dbbb2d32fb94
Carlos Eduardo <2005carloseduardosantos@gmail.com>
2023-06-13 00:25:19 -0300

    2023

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+function linguagens(){
+    var html = document.getElementsByClassName('.html') ;
+    var css =  document.querySelector('.css');
+    var js =  document.querySelector('.js');
+    var boot = document.querySelector('.boot'); 
+
+    css.classList.add('css-ani')
+
+}
