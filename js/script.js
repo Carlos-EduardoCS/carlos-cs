130e6e3ec8359785abc1a1d27c82ee4642766105
Carlos Eduardo <2005carloseduardosantos@gmail.com>
2023-06-13 12:00:37 -0300

    add js :)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,93 @@
 
+// geral
+window.sr = ScrollReveal({ 
+    resete: true, 
+    duration: 2500,
+    distance:'60px',
+    
+});
 
-function linguagens(){
-    var html = document.getElementsByClassName('.html') ;
-    var css =  document.querySelector('.css');
-    var js =  document.querySelector('.js');
-    var boot = document.querySelector('.boot'); 
 
-    css.classList.add('css-ani')
+sr.reveal('i', {
+    delay: 900,
+    origin: 'top',
+    duration: 1000,
+});
 
-}
+
+//photo
+sr.reveal('.carlos', {
+    rotate: {x: 0, y:80, z:0},
+    delay: 200,
+    duration: 2000,
+    origin: 'rigth',
+});
+
+//titlle
+sr.reveal('.in-start', {
+    duration: 2500,
+    origin: 'left',
+    delay: 250,
+});
+
+//skils-box
+sr.reveal('.in-skils', {
+    duration: 1000,
+    interval: 2000,
+})
+
+sr.reveal('.boot',{
+    origin: 'left',
+    delay: 900,
+})
+
+sr.reveal('.html',{
+    origin: 'left',
+    delay: 900,
+})
+
+sr.reveal('.css',{
+    origin: 'left',
+    delay: 900,
+})
+
+sr.reveal('.js',{
+    origin: 'left',
+    delay: 900,
+})
+
+sr.reveal('.skils-h1',{
+    origin: 'left',
+    delay: 500,
+})
+
+sr.reveal('.btn1',{
+    distance:'15px',
+    duration: 1000,
+    origin: 'top',
+    delay: 700,
+    interval: 2000,
+})
+
+sr.reveal('.txt-instart',{
+    distance:'20px',
+    origin: 'rigth',
+    duration: 1000,
+    delay: 500,
+})
+
+sr.reveal('.port-h1',{
+    distance:'0px',
+    origin: 'top',
+    duration: 1000,
+    delay: 500,
+})
+
+
+
+
+
+
+
+
+
